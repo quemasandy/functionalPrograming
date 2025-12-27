6b34fe4d39296c466a1088d79aad8e216209a302
Andrés Muñoz <andres.munoz@idukay.com>
2025-12-27 16:21:20 -0500

    feat: Introduce functional programming exercises and concepts across multiple learning modules.

---
+++ src/01-fundamentals/01-exercises/02-race-conditions-retries/my-single-retry.ts
@@ -0,0 +1,273 @@
+type CuentaBancaria = {
+  id: string;
+  titular: string;
+  saldo: number;
+  version: number;
+};
+
+type ITransaction = {
+  id: string,
+  cuentaId: string,
+  monto: number
+}
+
+type ResultDb<T, E> =
+  | { success: true; data: T }
+  | { success: false; error: E }
+
+type ErrorResultTransaction<E> = { success: false; error: E } & ITransaction;
+
+type ResultTransaction<T, E> =
+  | { success: true; data: { transactionId: string, account: T } }
+  | ErrorResultTransaction<E>;
+
+type DbErrorType = 'versionMismatch' | 'accountNotFound' | 'accountAlreadyExists';
+type TransactionErrorType = 'insufficientFunds' | DbErrorType;
+
+interface IDatabase {
+  create(id: string, data: CuentaBancaria): Promise<ResultDb<CuentaBancaria, DbErrorType>>;
+  getById(id: string): Promise<ResultDb<CuentaBancaria, DbErrorType>>;
+  updateIfVersionMatches(id: string, data: CuentaBancaria): Promise<ResultDb<CuentaBancaria, DbErrorType>>;
+}
+
+class MemoryDb implements IDatabase {
+  private db: Map<string, CuentaBancaria> = new Map();
+
+  constructor() {
+    this.db = new Map();
+  }
+
+  async create(id: string, data: CuentaBancaria): Promise<ResultDb<CuentaBancaria, DbErrorType>> {
+    if (this.db.has(id)) {
+      return { success: false, error: 'accountAlreadyExists' };
+    }
+    this.db.set(id, data);
+    return { success: true, data };
+  }
+
+  async getById(id: string): Promise<ResultDb<CuentaBancaria, DbErrorType>> {
+    const cuenta = this.db.get(id);
+    if (!cuenta) {
+      return { success: false, error: 'accountNotFound' };
+    }
+    return { success: true, data: cuenta };
+  }
+
+  async updateIfVersionMatches(id: string, data: CuentaBancaria): Promise<ResultDb<CuentaBancaria, DbErrorType>> {
+    const cuenta = this.db.get(id);
+    if (!cuenta) {
+      return { success: false, error: 'accountNotFound' };
+    }
+    if (cuenta.version !== data.version) {
+      return { success: false, error: 'versionMismatch' };
+    }
+
+    const updatedCuenta = {
+      ...data,
+      version: data.version + 1,
+    };
+
+    this.db.set(id, updatedCuenta);
+    return { success: true, data: updatedCuenta };
+  }
+}
+
+class CustomError extends Error {
+  public readonly transactionId: string;
+
+  constructor(message: string, transaction: ITransaction) {
+    super(message);
+    this.transactionId = transaction.id;
+  }
+
+  toString() {
+    return `Error: ${this.message} - transactionId: ${this.transactionId}`;
+  }
+}
+
+async function retirarDineroVersionControl(
+  transaction: ITransaction,
+  db: IDatabase
+): Promise<ResultTransaction<CuentaBancaria, TransactionErrorType>> {
+  console.log(`Iniciando transacción ${transaction.id}`);
+  // PASO 1: Leemos el estado actual (LECTURA)
+  const cuenta = await db.getById(transaction.cuentaId);
+
+  if (!cuenta.success) {
+    return { success: false, error: cuenta.error, ...transaction };
+  }
+
+  // simular error del server
+  if (transaction.id === '002') {
+    throw new CustomError('serverError', transaction);
+  }
+  // Simulamos latencia de red/base de datos
+  await sleep(100); // ⚠️ AQUÍ ESTÁ EL PELIGRO
+
+  // PASO 2: Validamos la regla de negocio
+  if (transaction.monto > cuenta.data.saldo) {
+    return { success: false, error: 'insufficientFunds', ...transaction };
+  }
+
+  // PASO 3: Calculamos el nuevo estado
+  const nuevoSaldo = cuenta.data.saldo - transaction.monto;
+
+  // Más latencia simulada
+  await sleep(50);
+
+  const updatedCuenta = await db.updateIfVersionMatches(transaction.cuentaId, {
+    id: transaction.cuentaId,
+    titular: cuenta.data.titular,
+    saldo: nuevoSaldo,
+    version: cuenta.data.version,
+  });
+
+  if (!updatedCuenta.success) {
+    return { success: false, error: updatedCuenta.error, ...transaction };
+  }
+
+  return {
+    success: true,
+    data: {
+      transactionId: transaction.id,
+      account: updatedCuenta.data,
+    },
+  };
+}
+
+function sleep(ms: number): Promise<void> {
+  return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function demostrarRaceConditionVersion(
+  transactions: { id: string, cuentaId: string, monto: number }[],
+  db: IDatabase
+): Promise<{
+  fulfilled: ResultTransaction<CuentaBancaria, TransactionErrorType>[],
+  rejected: CustomError[]
+}
+> {
+  const promises = transactions.map(transaction =>
+    retirarDineroVersionControl(transaction, db)
+  );
+
+  const results = await Promise.allSettled(promises);
+
+  const fulfilled = results.filter(result => result.status === 'fulfilled').map(result => result.value)
+  const rejected = results.filter(result => result.status === 'rejected').map(result => result.reason)
+
+  return {
+    fulfilled,
+    rejected
+  }
+}
+
+function jitter() {
+  return Math.floor(Math.random() * 1000)
+}
+
+async function retirarDineroResiliente(
+  transactions: { id: string, cuentaId: string, monto: number }[],
+  db: IDatabase,
+  options?: { maxRetries: number }
+): Promise<{
+  fulfilled: {retry: number, response: ResultTransaction<CuentaBancaria, TransactionErrorType>[]}[],
+  rejected: {retry: number, response: CustomError[]}[]
+}> {
+
+  const maxRetries = options?.maxRetries || 3;
+
+  const fulfilledTransactions: {retry: number, response: ResultTransaction<CuentaBancaria, TransactionErrorType>[]}[] = [];
+  const rejectedTransactions: {retry: number, response: CustomError[]}[] = [];
+
+  for (let i = 0; i < maxRetries; i++) {
+    await sleep(jitter())
+    
+    const { fulfilled, rejected } = await demostrarRaceConditionVersion(transactions, db)
+    
+    fulfilledTransactions.push({retry: i, response: fulfilled})
+    rejectedTransactions.push({retry: i, response: rejected})
+
+    const versionMismatchResponses = fulfilled.filter(
+      result => result.success === false && result.error === 'versionMismatch'
+    ) as ErrorResultTransaction<TransactionErrorType>[];
+
+    if (versionMismatchResponses.length === 0) {
+      break;
+    }
+
+    await sleep((2 ** i) * 1000)
+
+    transactions = versionMismatchResponses.map(result => (
+      { id: result.id, cuentaId: result.cuentaId, monto: result.monto }
+    ))
+  }
+
+  return {
+    fulfilled: fulfilledTransactions,
+    rejected: rejectedTransactions
+  }
+}
+
+async function main() {
+  console.log('='.repeat(70));
+  console.log('❌ DEMOSTRACIÓN: VERSION LOCKING EN ACCIÓN');
+  console.log('='.repeat(70));
+
+  console.log('📊 Inicializando base de datos');
+  console.log('🎯 Intentando retiros simultáneos...');
+
+  const db = new MemoryDb();
+
+  db.create('cuenta-001', {
+    id: 'cuenta-001',
+    titular: 'Juan',
+    saldo: 2500,
+    version: 0,
+  });
+
+  const transactions = [
+    { id: '001', cuentaId: 'cuenta-001', monto: 800 },
+    { id: '002', cuentaId: 'cuenta-001', monto: 800 },
+    { id: '003', cuentaId: 'cuenta-001', monto: 800 },
+    { id: '004', cuentaId: 'cuenta-001', monto: 800 },
+    { id: '005', cuentaId: 'cuenta-001', monto: 800 },
+  ]
+
+  const { fulfilled, rejected } = await retirarDineroResiliente(transactions, db)
+
+  console.log('✅ Transacciones procesadas:');
+  fulfilled.forEach((result) => {
+    console.log(`Retry Attempt: ${result.retry}`)
+    result.response.forEach((response) => {
+      const txId = response.success ? response.data.transactionId : response.id;
+      console.log(`👤 tx ${txId}: ${JSON.stringify(response, null, 2)}`);
+    })
+  })
+
+  console.log('❌ Transacciones fallidas:');
+  rejected.forEach((result) => {
+    console.log(`Retry Attempt: ${result.retry}`)
+    if (result.response.length === 0) {
+      console.log('Sin Errores');
+    }
+    result.response.forEach((response) => {
+      console.log(`👤 tx ${response.transactionId}: ${response.toString()}`);
+    })
+  })
+
+  const cuentaFinal = await db.getById('cuenta-001');
+
+  if (!cuentaFinal.success) {
+    return cuentaFinal;
+  }
+
+  console.log('='.repeat(70));
+  console.log(`💰 Saldo final: $${cuentaFinal.data.saldo}`);
+  console.log(`⚠️  Prueba Finalizada.`);
+  console.log('='.repeat(70));
+}
+
+main();
+
+export { };
